9d08cae637d06adbc007db3a03354c23bdad20c8
Soham Kulkarni <sohamkrox@gmail.com>
2023-11-04 21:00:02 -0500

    But buttons inside image

--- src/components/remage.tsx
+++ src/components/remage.tsx
@@ -22,37 +22,38 @@ export const Remage = ({
   const [state, setState] = useState(initialState);
 
   return (
-    <TransformWrapper
-      initialScale={initialState.scale}
-      maxScale={20}
-      ref={transformComponentRef}
-      onTransformed={(_a, newState) => {
-        setState(newState);
-      }}
-    >
-      {({ zoomIn, zoomOut, resetTransform }) => (
-        <React.Fragment>
-          <div className="tools">
-            <button onClick={() => zoomIn()}>+</button>
-            <button onClick={() => zoomOut()}>-</button>
-            <button onClick={() => resetTransform()}>Reset</button>
-          </div>
-
-          <TransformComponent>
-            <img src={src} alt="" style={{ position: "relative" }} />
-            {interactables.map((int) => {
-              return (
-                <div
-                  key={`${int.left}-${int.top}`}
-                  style={{ left: int.left, top: int.top, position: "fixed" }}
-                >
-                  {int.interactable(state)}
-                </div>
-              );
-            })}
-          </TransformComponent>
-        </React.Fragment>
-      )}
-    </TransformWrapper>
+    <div className="w-1/2">
+      <TransformWrapper
+        initialScale={initialState.scale}
+        maxScale={20}
+        ref={transformComponentRef}
+        onTransformed={(_a, newState) => {
+          setState(newState);
+        }}
+      >
+        {({ zoomIn, zoomOut, resetTransform }) => (
+          <React.Fragment>
+            <TransformComponent>
+              <div className="fixed bottom-0 right-0 z-30">
+                <button onClick={() => zoomIn()}>+</button>
+                <button onClick={() => zoomOut()}>-</button>
+                <button onClick={() => resetTransform()}>Reset</button>
+              </div>
+              <img src={src} alt="" style={{ position: "relative" }} />
+              {interactables.map((int) => {
+                return (
+                  <div
+                    key={`${int.left}-${int.top}`}
+                    style={{ left: int.left, top: int.top, position: "fixed" }}
+                  >
+                    {int.interactable(state)}
+                  </div>
+                );
+              })}
+            </TransformComponent>
+          </React.Fragment>
+        )}
+      </TransformWrapper>
+    </div>
   );
 };
