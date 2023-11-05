68db2ea02416149181ec563437f7b4da4438beeb
Soham Kulkarni <sohamkrox@gmail.com>
2023-11-04 22:57:02 -0500

    Add EOHMap to its own file

---
+++ src/remages/eoh-map.tsx
@@ -0,0 +1,57 @@
+import { useState } from "react";
+import { Remage } from "../components/remage";
+import { SolidPinInteract } from "../interacts/solidpin";
+
+export const EOHMap = () => {
+  const [search, setSearch] = useState("");
+
+  return (
+    <div>
+      <input type="text" onChange={(e) => setSearch(e.target.value)} />
+
+      <Remage
+        src="campus_map.png"
+        interactables={[
+          SolidPinInteract({
+            top: "15%",
+            left: "45%",
+            popupText: "National Center for Supercomputing Applications",
+            tags: "National Center for Supercomputing Applications",
+          }),
+          SolidPinInteract({
+            top: "27%",
+            left: "30%",
+            popupText: "Hydrosystems Lab",
+            tags: "Hydrosystems Lab",
+          }),
+          SolidPinInteract({
+            top: "30%",
+            left: "55%",
+            popupText: "Siebel Center",
+            tags: "Siebel Center",
+          }),
+          SolidPinInteract({
+            top: "35%",
+            left: "40%",
+            popupText: "Newmark (Crane Bay)",
+            tags: "Newmark (Crane Bay)",
+          }),
+          SolidPinInteract({
+            top: "43%",
+            left: "45%",
+            popupText: "Digital Computer Laboratory",
+            tags: "Digital Computer Laboratory",
+          }),
+          SolidPinInteract({
+            top: "72%",
+            left: "47%",
+            popupText: "Everitt Laboratory",
+            tags: "Everitt Laboratory",
+          }),
+        ].filter((intr) =>
+          intr.tags.toLocaleLowerCase().includes(search.toLocaleLowerCase())
+        )}
+      />
+    </div>
+  );
+};
